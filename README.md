7d7b1f775e91e4c46cec07bbf46f1f305c257bbb
chocofalls <chocofallsdev@gmail.com>
2021-06-10 23:32:35 +0300

    more improvements and posting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # defistation-pusher
+
 This app is working for push macaronswap tvl data to defistation.
+
+**Need to fill .env.template file with variables and make its name .env**
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ var Cron = require("node-cron");
 const clientID = process.env.DEFISTATION_API_CLIENT_ID;
 const clientSecret = process.env.DEFISTATION_API_CLIENT_SECRET;
 
-let authorizationBasic;
+let authorizationBasic: string;
 if (clientID && clientSecret) {
   authorizationBasic = Buffer.from(
     clientID + ":" + clientSecret,
@@ -21,7 +21,7 @@ if (clientID && clientSecret) {
 }
 
 Signale.start(`Initalized DefiStation Pusher @[${new Date().toISOString()}]`);
-Cron.schedule("50 * * * *", () => {
+Cron.schedule("* * * * *", () => {
   // Fetches data at *:50
   taskRunner();
 });
@@ -33,9 +33,21 @@ const taskRunner = async () => {
         Signale.success(
           `TVL data fetched successfully @[${new Date().toISOString()}]`,
         );
-        Signale.success(
-          `TVL data pushed to DefiStation successfully @[${new Date().toISOString()}]`,
-        );
+        if (data.updated_at) {
+          delete data.updated_at;
+        }
+        postTVLData(data)
+          .then((resp: any) => {
+            Signale.success(
+              `TVL data posted successfully @[${new Date().toISOString()}]`,
+            );
+          })
+          .catch((e: any) => {
+            Signale.error(
+              `TVL data can't posted skipping @[${new Date().toISOString()}] 
+              Error: ${e}`,
+            );
+          });
       } else {
         Signale.error(
           `TVL data came empty from MacaronSwap api @[${new Date().toISOString()}]`,
@@ -44,36 +56,62 @@ const taskRunner = async () => {
     })
     .catch((e: any) => {
       Signale.error(
-        `TVL data can't fetched skipping @[${new Date().toISOString()}]`,
+        `TVL data can't fetched skipping @[${new Date().toISOString()}]
+        Error: ${e}`,
       );
     });
 };
 
-const getTVLData = () => {
+const getTVLData = async () => {
   if (process.env.MACARONSWAP_API_TVL_URL) {
     const apiTVLURL = process.env.MACARONSWAP_API_TVL_URL;
     Signale.start(`Starting api TVL fetch @[${new Date().toISOString()}]`);
     Signale.time("Api TVL Fetch");
-    return axios
-      .get(apiTVLURL)
-      .then((response: any) => {
-        Signale.timeEnd("Api TVL Fetch");
-        return response.data;
-      })
-      .catch((e: any) => {
-        Signale.timeEnd("Api TVL Fetch");
-        Signale.error(
-          `An error occured when trying to fetch MacaronSwap TVL from url: ${apiTVLURL} @[${new Date().toISOString()}]
+    try {
+      const response = await axios.get(apiTVLURL);
+      Signale.timeEnd("Api TVL Fetch");
+      return response.data;
+    } catch (e) {
+      Signale.timeEnd("Api TVL Fetch");
+      Signale.error(
+        `An error occured when trying to fetch MacaronSwap TVL from url: ${apiTVLURL} @[${new Date().toISOString()}]
           ${e}`,
-        );
-        throw e;
-      });
+      );
+      throw e;
+    }
   } else {
-    Signale.error(
+    Signale.fatal(
       "MacaronSwap Api TVL URL does not exist in env variables. Can't fetch!",
     );
+    process.exit(1);
   }
 };
 
-//TODO: Implement sending data to DefiStation
-const postTVLData = () => {};
+const postTVLData = async (data: any) => {
+  if (process.env.DEFISTATION_API_TVL_URL) {
+    const apiTVLURL = process.env.DEFISTATION_API_TVL_URL;
+    Signale.start(`Starting api TVL post @[${new Date().toISOString()}]`);
+    Signale.time("Api TVL Post");
+    try {
+      const resp = await axios.post(apiTVLURL, data, {
+        headers: {
+          Authorization: `Basic ${authorizationBasic}`,
+        },
+      });
+      Signale.timeEnd("Api TVL Post");
+      return resp;
+    } catch (e) {
+      Signale.timeEnd("Api TVL Post");
+      Signale.error(
+        `An error occured when trying to post MacaronSwap TVL to url: ${apiTVLURL} @[${new Date().toISOString()}]
+          ${e}`,
+      );
+      throw e;
+    }
+  } else {
+    Signale.fatal(
+      "DefiStation Api TVL URL does not exist in env variables. Can't fetch!",
+    );
+    process.exit(1);
+  }
+};
